8fd5a4d48a3ceb3fb0ca7fe1cf49d3a6ea8772af
marcelin <marcelintingougoui3@gmail.com>
2025-10-09 17:11:36 +0200

    L'ajout de la gestion des  profils le controller, service et router.

--- src/routers/auth.route.js
+++ src/routers/auth.route.js
@@ -1,9 +1,19 @@
-import { registeruser,loginuser } from '../controllers/auth.controller.js';
+import { registeruser,loginuser,getalluser,getuserbyid,edituser,deleteuser } from '../controllers/auth.controller.js';
 import express from 'express';
 const router = express.Router();
 
 
 router.post('/register', registeruser);
 router.post('/login',loginuser)
+router.get('/get/all/users',getalluser)
+router.get('/profile/id',getuserbyid)
+router.patch('/profile/edit/id',edituser)
+router.delete('/profile/delete',deleteuser)
+
+// POST | /api/profile/add | Créer un profil secondaire | authMiddleware
+// GET | /api/profile | Voir ses profils | authMiddleware
+// PUT | /api/profile/edit/:id | Modifier un profil | authMiddleware
+// DELETE | /api/profile/delete/:id | Supprimer un profil | authMiddleware
+
 
 export default router;
