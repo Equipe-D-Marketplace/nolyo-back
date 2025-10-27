e1bcf4d10240fd573772be4210caf3ab8fd6032e
marcelin <marcelintingougoui3@gmail.com>
2025-10-27 16:05:03 +0100

    Mise à jour de la configuration de la base de données vers PostgreSQL, ajout de la gestion des utilisateurs avec middleware d'authentification et mise à jour des routes d'authentification. PROFIL (CRUD multi-profil) Fixes #16

--- src/routers/auth.route.js
+++ src/routers/auth.route.js
@@ -1,14 +1,15 @@
 import { registeruser,loginuser,getalluser,getuserbyid,edituser,deleteuser } from '../controllers/auth.controller.js';
 import express from 'express';
 const router = express.Router();
+import authMiddleware from '../middleware/all.middleware.js';
 
 
 router.post('/register', registeruser);
 router.post('/login',loginuser)
-router.get('/get/all/users',getalluser)
-router.get('/profile/id',getuserbyid)
-router.patch('/profile/edit/id',edituser)
-router.delete('/profile/delete',deleteuser)
+router.get('/get/all/users',authMiddleware,getalluser)
+router.get('/profile/id',authMiddleware,getuserbyid)
+router.patch('/profile/edit/id',authMiddleware,edituser)
+router.delete('/profile/delete',authMiddleware,deleteuser)
 
 // POST | /api/profile/add | Créer un profil secondaire | authMiddleware
 // GET | /api/profile | Voir ses profils | authMiddleware
