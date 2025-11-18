59888d8ed6f136ca9ade36809d9e6f760892360c
TINGOUGOUI M Marcelin <87518783+Marcelin69@users.noreply.github.com>
2025-11-18 12:42:14 +0100

    Update product.route.js

--- src/routers/product.route.js
+++ src/routers/product.route.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 // Routes CRUD pour les produits
 router.get('/', getAllProducts);           // GET /api/products
-router.get('/:id', authMiddleware, getProductById);        // GET /api/products/:id
+router.get('/:id', getProductById);        // GET /api/products/:id
 router.post('/', authMiddleware, createProduct);          // POST /api/products
 router.put('/:id', authMiddleware, updateProduct);        // PUT /api/products/:id
 router.delete('/:id', authMiddleware, deleteProduct);     // DELETE /api/products/:id
